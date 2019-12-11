e69d500a1dd0644889bca1e0cd527b8efcf140e7
Martin Stemplinger <mstempl@gmail.com>
2019-12-11 21:04:10 +0100

    Added JSON-Navigator

--- custom.el
+++ custom.el
@@ -6,24 +6,23 @@
  '(line-spacing 0.2)
  '(org-export-backends '(ascii html md))
  '(package-selected-packages
-   '(paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering)))
+   '(json-navigator hc-zenburn-mode hc-zenburn paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
  '(all-the-icons-dired-dir-face ((t (:foreground nil))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
- '(fixed-pitch ((t (:family "Consolas" :slant normal :weight normal :height 0.9 :width normal))))
+ '(fixed-pitch ((t (:family "monofur" :slant normal :weight normal :height 0.9 :width normal))))
  '(org-block ((t (:inherit fixed-pitch))))
  '(org-column ((t (:background "light gray" :strike-through nil :underline nil :slant normal :weight normal))))
  '(org-column-title ((t (:background "gray" :underline t :weight bold))))
  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
  '(org-document-title ((t (:inherit default :weight bold :height 2.0 :underline nil))))
  '(org-headline-done ((t (:inherit variable-pitch :foreground "white" :strike-through t))))
- '(org-indent ((t  (:inherit (org-hide fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
  '(org-level-1 ((t (:inherit variable-pitch :weight bold :height 1.4))))
  '(org-level-2 ((t (:inherit variable-pitch :weight bold :height 1.3))))
  '(org-level-3 ((t (:inherit variable-pitch :weight bold :height 1.2))))
@@ -32,7 +31,7 @@
  '(org-level-6 ((t (:inherit variable-pitch :weight bold))))
  '(org-level-7 ((t (:inherit variable-pitch :weight bold))))
  '(org-level-8 ((t (:inherit variable-pitch :weight bold))))
- '(org-link ((t (:foreground "blue" :underline t))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-property-value ((t (:inherit fixed-pitch))) t)
  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
@@ -40,3 +39,4 @@
  '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
  '(taskpaper-tag ((t (:inherit font-lock-keyword-face))))
  '(variable-pitch ((t (:family "ETBembo")))))
+; '(variable-pitch ((t (:family "Source Sans Pro" :height 120 :weight light)))))
