78d3e025a95f8f8c25ac2d358db79e84a4d69719
Martin Stemplinger <mstempl@gmail.com>
2019-12-20 17:42:28 +0100

    Font setting changes

--- custom.el
+++ custom.el
@@ -3,10 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(line-spacing 0.2)
  '(org-export-backends '(ascii html md))
  '(package-selected-packages
-   '(json-navigator hc-zenburn-mode hc-zenburn paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering)))
+   '(arjen-grey-theme json-navigator hc-zenburn-mode hc-zenburn paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -15,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(all-the-icons-dired-dir-face ((t (:foreground nil))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
- '(fixed-pitch ((t (:family "monofur" :slant normal :weight normal :height 0.9 :width normal))))
+ '(fixed-pitch ((t (:family "monofur" :slant normal :weight normal :height 113 :width normal))))
  '(org-block ((t (:inherit fixed-pitch))))
  '(org-column ((t (:background "light gray" :strike-through nil :underline nil :slant normal :weight normal))))
  '(org-column-title ((t (:background "gray" :underline t :weight bold))))
@@ -38,5 +37,5 @@
  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
  '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
  '(taskpaper-tag ((t (:inherit font-lock-keyword-face))))
- '(variable-pitch ((t (:family "ETBembo")))))
+ '(variable-pitch ((t (:family "ETBembo" :weight normal :height 113 :width normal)))))
 ; '(variable-pitch ((t (:family "Source Sans Pro" :height 120 :weight light)))))
