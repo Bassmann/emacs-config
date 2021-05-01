c131f0c68efd9c8fe38c75ba1c12cb60763e3b84
Martin Stemplinger <mstempl@gmail.com>
2021-05-01 21:49:01 +0200

    Switch to Nord theme

--- custom.el
+++ custom.el
@@ -3,10 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-message-face 'default t)
  '(org-export-backends '(ascii html md))
  '(package-selected-packages
-   '(org-superstar dired-narrow org-roam-server emacsql-sqlite3 emacsql company-org-roam org-roam org-books org-download doct modus-vivendi-theme modus-operandi-theme org-noter pdf-tools org-recent-headings mixed-pitch color-theme-sanityinc-tomorrow arjen-grey-theme json-navigator hc-zenburn-mode hc-zenburn paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering))
+   '(nord-theme org-pdftools org-superstar dired-narrow emacsql-sqlite3 emacsql org-books org-download doct modus-vivendi-theme modus-operandi-theme org-noter pdf-tools org-recent-headings mixed-pitch color-theme-sanityinc-tomorrow arjen-grey-theme json-navigator hc-zenburn-mode hc-zenburn paper-theme grip-mode org-sidebar powershell plantuml-mode php-mode moody minions helpful dired-git-info parchment-theme calfw-cal material-theme system-packages org-kanban openwith py-autopep8 elpy centaur-tabs filetags date2name ztree org-fancy-priorities calfw-ical calfw-org calfw org-pretty-tags all-the-icons-ivy disk-usage nov treemacs-icons-dired swiper use-package-ensure-system-package visual-fill diredfl dired-hide-dotfiles dired-toggle paradox org-outline-numbering delight bind-key org-plus-contrib olivetti ssh-agency ox-hugo-auto-export ox-hugo org-wiki org-mind-map org-index dired-filetype-face org-preview-html outline-toc company-box ivy-rich counsel flymd doom-themes web-mode comment-dwim-2 rainbow-mode magit markdown org2jekyll s ivy mode-icons poet-theme toc-org org-expiry neotree all-the-icons-dired all-the-icons dired-sidebar dired-subtree golden-ratio-scroll-screen solarized-theme ace-window which-key todotxt-mode org-brain org-cliplink link-hint default-text-scale dashboard rainbow-delimiters diminish auto-revert htmlize powerline uniquify deft ox-pandoc ac-html ac-html-bootstrap company-web zenburn-theme undo-tree smex smart-mode-line scss-mode powershell-mode php-extras org-bullets org-ac markdown-mode less-css-mode js2-mode ido-ubiquitous hl-line+ hc-zenburn-theme flycheck feature-mode f exec-path-from-shell esxml dired-details company use-package no-littering))
  '(paradox-github-token t))
 
 (custom-set-faces
@@ -14,17 +13,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :width normal))))
  '(all-the-icons-dired-dir-face ((t (:foreground nil))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(fixed-pitch ((t (:family "Consolas" :height 0.9 :slant normal :weight normal :width normal))))
- '(org-block ((t (:inherit fixed-pitch))))
  '(org-column ((t (:background "light gray" :strike-through nil :underline nil :slant normal :weight normal))))
  '(org-column-title ((t (:background "gray" :underline t :weight bold))))
  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :height 2.0 :underline nil))))
  '(org-drawer ((t (:weight thin :foreground "#586e75"))))
- '(org-headline-done ((t (:inherit variable-pitch :strike-through t))))
  '(org-indent ((t (:inherit (org-hide)))))
  '(org-level-1 ((t (:inherit variable-pitch :weight bold :height 1.4))))
  '(org-level-2 ((t (:inherit variable-pitch :weight bold :height 1.3))))
@@ -35,13 +30,7 @@
  '(org-level-7 ((t (:inherit variable-pitch :weight bold))))
  '(org-level-8 ((t (:inherit variable-pitch :weight bold))))
  '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
  '(org-roam-link ((t (:foreground "lime green" :underline t))))
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
  '(taskpaper-tag ((t (:inherit font-lock-keyword-face))))
  '(variable-pitch ((t (:family "ETBembo" :height 113 :width normal)))))
 ; '(variable-pitch ((t (:family "Source Sans Pro" :height 120 :weight light)))))
